9bf0c550b656a9e029beadb1e173a9ba2e2cf4bd
GissellaAguilar <gis.aguilar@duocuc.cl>
2025-11-10 14:50:37 -0300

    navegacion funcionando

--- app/src/main/java/com/example/paseleriamilsabores/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/paseleriamilsabores/navigation/AppNavigation.kt
@@ -4,12 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.paseleriamilsabores.ui.screens.HomeScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = AppScreens.Home.route) {
-        composable(AppScreens.Home.route) { /* Aquí llamarás a tu HomeScreen() */ }
+        composable(AppScreens.Home.route) { HomeScreen(navController) }
         composable(AppScreens.Login.route) { /* Aquí llamarás a tu LoginScreen() */ }
         composable(AppScreens.Registro.route) { /* ... */ }
         composable(AppScreens.Tortas.route) { /* ... */ }
--- app/src/main/java/com/example/paseleriamilsabores/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/paseleriamilsabores/ui/screens/HomeScreen.kt
@@ -2,8 +2,9 @@ package com.example.paseleriamilsabores.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavController) {
     Text("¡Hola, esta es la pantalla de Inicio!")
 }
