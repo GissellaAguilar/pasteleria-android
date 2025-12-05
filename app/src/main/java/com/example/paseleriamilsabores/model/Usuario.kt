d4016b2a4f80834930d73ac3686ae913d6f6ed9d
GissellaAguilar <gis.aguilar@duocuc.cl>
2025-12-05 12:01:48 -0300

    model usuario

--- app/src/main/java/com/example/paseleriamilsabores/model/Usuario.kt
+++ app/src/main/java/com/example/paseleriamilsabores/model/Usuario.kt
@@ -1,4 +1,15 @@
 package com.example.paseleriamilsabores.model
 
-class Usuario {
-}
+class Usuario(
+    val run: String,
+    val nombre: String,
+    val apellido: String,
+    val correo: String,
+    val password: String,
+    val direccion: String,
+    val region: String,
+    val comuna: String,
+    val fechaNac: String,
+    val codigo: String,
+    val rol: String
+)
